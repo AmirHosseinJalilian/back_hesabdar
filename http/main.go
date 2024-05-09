0e32ad3e6d0c33f323755cc93ee4c43d9f336f04
AmirHosseinJalilian <jalilian.amirhossein@gmail.com>
2024-05-09 14:35:50 +0330

    fix cors error in main file and delete kit folder

--- http/main.go
+++ http/main.go
@@ -6,21 +6,24 @@ import (
 	"github.com/AmirHosseinJalilian/back_hesabdar/services/sale_factor_confirmation"
 	"github.com/AmirHosseinJalilian/back_hesabdar/services/sale_factor_confirmation_details"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/cors"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, // Allow all origins
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		Debug:          true,
+	})
+	// Wrap Echo instance with CORS handler
+	e.Use(echo.WrapMiddleware(c.Handler))
 	// Connect to the database
 	db := database.Connect()
 	// Remember to defer closing the database connection until application stops
 	defer db.Close()
-	// Routes
 	// Define routes
 	e.GET("/Pepoles", func(c echo.Context) error {
 		return pepole.GetPepoles(c, db)
@@ -31,18 +34,5 @@ func main() {
 	e.GET("/SaleFactorConfirmationDetails", func(c echo.Context) error {
 		return sale_factor_confirmation_details.GetSaleFactorConfirmationDetails(c, db)
 	})
-	e.Start(":8080")
-	// log.Fatal(http.ListenAndServe(serverPort, router))
-
-	// 	mux := http.NewServeMux()
-	// 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 		w.Header().Set("Content-Type", "application/json")
-	// 		w.Write([]byte("{\"hello\": \"world\"}"))
-	// 	})
-
-	// 	// cors.Default() setup the middleware with default options being
-	// 	// all origins accepted with simple methods (GET, POST). See
-	// 	// documentation below for more options.
-	// 	handler := cors.Default().Handler(mux)
-	// 	http.ListenAndServe(":8080", handler)
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- kit/exp/exp.go
+++
@@ -1,8 +0,0 @@
-package exp
-
-func TerIf[T any](cond bool, vtrue, vfalse T) T {
-	if cond {
-		return vtrue
-	}
-	return vfalse
-}
