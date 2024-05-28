fd3f3b2f26fdb0a6578da63d8fe13918c5ef3575
amir hossein <jalilian.amirhossein@gmail.com>
2024-05-28 15:56:08 +0330

    small change in connect and main

--- database/connect.go
+++ database/connect.go
@@ -10,24 +10,21 @@ import (
 
 func Connect() *gorm.DB {
 	const (
-		host     = "192.168.1.109"
+		host     = "192.168.43.190"
 		port     = "7007"
 		user     = "netim"     // Replace with your SQL Server username
 		password = "smj920123" // Replace with your SQL Server password
 		dbName   = "Mehrad"    // Replace with your SQL Server database name
 	)
 
-	// Construct the DSN (Data Source Name) for SQL Server
 	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s",
 		user, password, host, port, dbName)
 
-	// Connect to the SQL Server using GORM
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to open connection to SQL Server database: %v", err)
 	}
 
-	// Verify connection
 	sqlDB, err := db.DB()
 	if err != nil {
 		log.Fatalf("Failed to get generic database object: %v", err)
@@ -39,57 +36,3 @@ func Connect() *gorm.DB {
 	fmt.Println("Connected to SQL Server database using GORM.")
 	return db
 }
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	_ "github.com/denisenkom/go-mssqldb"
-// 	"log"
-// )
-
-// func Connect() *sql.DB {
-
-// 	const (
-// 		driverName = "sqlserver"
-// 		host       = "192.168.1.109"
-// 		// host     = "192.168.1.100"
-// 		port     = "7007"
-// 		user     = "netim"     // Replace with your SQL Server username
-// 		password = "smj920123" // Replace with your SQL Server password
-// 		dbName   = "Mehrad"    // Replace with your SQL Server database name
-// 	)
-
-// 	var db *sql.DB
-
-// 	// Connect to the SQL Server
-// 	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s",
-// 		user, password, host, port, dbName)
-// 	var err error
-// 	db, err = sql.Open(driverName, dsn)
-// 	if err != nil {
-// 		log.Fatalf("Failed to open connection to SQL Server database: %v", err)
-// 	}
-// 	// defer db.Close()
-
-// 	// Verify connection
-// 	if err = db.Ping(); err != nil {
-// 		log.Fatalf("Failed to ping SQL Server database: %v", err)
-// 	}
-// 	fmt.Println("Connected to SQL Server database.")
-
-// 	// Create a new router
-// 	// router := mux.NewRouter()
-
-// 	// Define routes
-// 	// router.HandleFunc("/SaleFactorConfirmations", getSaleFactorConfirmations).Methods("GET", "OPTIONS")
-
-// 	// Start the server
-// 	serverPort := ":8080"
-// 	fmt.Printf("Server started on port %s\n", serverPort)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	// log.Fatal(http.ListenAndServe(serverPort, router))
-
-// 	return db
-// }
--- http/main.go
+++ http/main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"}, // Allow all origins
@@ -17,10 +16,8 @@ func main() {
 		AllowedHeaders: []string{"*"},
 		Debug:          true,
 	})
-	// Wrap Echo instance with CORS handler
 	e.Use(echo.WrapMiddleware(c.Handler))
 
-	// Connect to the database using GORM
 	db := database.Connect()
 	sqlDB, err := db.DB()
 	if err != nil {
@@ -28,44 +25,11 @@ func main() {
 	}
 	defer sqlDB.Close()
 
-	// Auto migrate the models
 	db.AutoMigrate(&models.SaleFactorConfirmation{}, &models.SaleFactorConfirmationDetails{}, &models.Commoditym{}, &models.Grouping{}, &models.Pepole{}, &models.PepoleDescription{})
 
-	// Define routes
 	e.GET("/SaleFactorConfirmations", func(c echo.Context) error {
 		return sale_factor_confirmation.GetSaleFactorConfirmations(c, db)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
-
-// import (
-// 	"github.com/AmirHosseinJalilian/back_hesabdar/database"
-// 	"github.com/AmirHosseinJalilian/back_hesabdar/services/sale"
-// 	"github.com/labstack/echo/v4"
-// 	"github.com/rs/cors"
-// )
-
-// func main() {
-// 	// Echo instance
-// 	e := echo.New()
-// 	c := cors.New(cors.Options{
-// 		AllowedOrigins: []string{"*"}, // Allow all origins
-// 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-// 		AllowedHeaders: []string{"*"},
-// 		Debug:          true,
-// 	})
-// 	// Wrap Echo instance with CORS handler
-// 	e.Use(echo.WrapMiddleware(c.Handler))
-// 	// Connect to the database
-// 	db := database.Connect()
-// 	// Remember to defer closing the database connection until application stops
-// 	defer db.Close()
-// 	// Define routes
-
-// 	e.GET("/Sale", func(c echo.Context) error {
-// 		return sale.GetSale(c, db)
-// 	})
-
-// 	e.Logger.Fatal(e.Start(":8080"))
-// }
